85f6149c118b7f97144dc79437a0cacacf2b9550
jl <jorgetrc01@gmail.com>
2022-06-13 16:36:27 -0300

    Network

---
+++ network.tf
@@ -0,0 +1,33 @@
+resource "oci_core_vcn" "vcn_terra" {
+    #Required
+    compartment_id = var.compartment_ocid
+    cidr_blocks = ["10.0.0.0/16", "172.16.0.0/16"]
+    display_name = "vcn_terra"
+}
+
+resource "oci_core_subnet" "subnet_terra" {
+    #Required
+    cidr_block = "10.0.1.0/24"
+    compartment_id = var.compartment_ocid
+    vcn_id = oci_core_vcn.vcn_terra.id
+    display_name = "subnet_terra"
+}
+
+resource "oci_core_internet_gateway" "terra_internet_gateway" {
+    #Required
+    compartment_id = var.compartment_ocid
+    vcn_id = oci_core_vcn.vcn_terra.id
+    display_name = "igw_terra"
+}
+
+resource "oci_core_route_table" "terra_route_table" {
+    #Required
+    compartment_id = var.compartment_ocid
+    vcn_id = oci_core_vcn.vcn_terra.id
+
+    route_rules {
+        #Required
+        network_entity_id = oci_core_internet_gateway.terra_internet_gateway.id
+        destination = "0.0.0.0/0"
+    }
+}
