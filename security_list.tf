6e917914aeeccb780fdee6b182d7bdc8de00d0ce
jl <jorgetrc01@gmail.com>
2022-06-13 18:44:32 -0300

    Security List

---
+++ security_list.tf
@@ -0,0 +1,30 @@
+resource "oci_core_security_list" "security_list_terra" {
+	compartment_id = var.compartment_ocid
+	vcn_id = oci_core_vcn.vcn_terra.id
+	display_name = "SL_terra"
+
+	egress_security_rules {
+		destination = "0.0.0.0/0"
+		protocol = "all"
+		}
+
+
+	ingress_security_rules {
+		protocol = "6"
+		source = "0.0.0.0/0"
+		tcp_options {
+			min = "22"
+			max = "22"
+		}
+	}
+
+		ingress_security_rules {
+                protocol = "6"
+                source = "0.0.0.0/0"
+                tcp_options {
+                        min = "8080"
+                        max = "8080"
+		}
+
+}	
+}
