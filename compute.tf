34cbb423f9e94b51e326bb08fcecdf6862c780be
jl <jorgetrc01@gmail.com>
2022-06-24 18:07:21 -0300

    Update display_name

--- compute.tf
+++ compute.tf
@@ -3,7 +3,7 @@ resource "oci_core_instance" "instance_server" {
 	availability_domain = "JNid:SA-SAOPAULO-1-AD-1"
 	compartment_id = var.compartment_ocid
 	shape = "VM.Standard.E2.1.Micro"
-	display_name = "server ${count.index +1}"
+	display_name = "server${count.index +1}"
 
 	source_details {
 	  source_id = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaayatkweelfzdxyceqxuqomh4fz72dnnzqwan46gyrsooytfwym47q"
