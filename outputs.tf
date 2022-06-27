0f962619184bf5f5067b50b895f095b481f664fe
jl <jorgetrc01@gmail.com>
2022-06-27 18:40:55 -0300

    Outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "Public_IP_Load_Balancer" {
+  value = [oci_load_balancer.load_balancer.ip_addresses]
+}
+output "Public_IP_server1" {
+  value	= [oci_core_instance.instance_server[0].public_ip]
+}
+output "Public_IP_server2" {
+  value = [oci_core_instance.instance_server[1].public_ip]
+} 
